8c507048c2b98512bfb2523236e36f48fb81dcf6
Logan Oram <logan.oram@keyin.com>
2024-06-28 00:28:23 -0230

    June28

--- SPRINT-1/scripts/info.js
+++ SPRINT-1/scripts/info.js
@@ -1,28 +1,12 @@
-window.addEventListener("DOMContentLoaded", function () {
-    let form = document.querySelector("#form1");
 
-    form.addEventListener("submit", function (e){
-       // go and collect user input from three input boxes and store them in js variables here
-      let quantity = document.querySelector("#quantity").value;
-      let fullname = document.querySelector("#fullname").value;
-      let phoneoremail = document.querySelector("#phoneoremail").value;
-      let cardnumber = document.querySelector("#cardnumber").value;
-
-      addItemtoList(quantity, fullname, phoneoremail, cardnumber);
-
-      e.preventDefault();
-    });
-
-    let submit = document.querySelector("#submit");
-
-    submit.addEventListener("click", function (e) {
-        if(q === "" | fn === "" || poe === "" || cn === "") {
-            alert("No field should be empty", "error")
-        } if(cn.length !== 16 || isNaN(c)) {
-            alert("Card Number must be a 16-digit number.")
-        } if (isNaN(q) || Number(q) < 0) {
-            alert("Quantities cannot be negative numbers.")
-        }
+document.addEventListener('DOMContentLoaded', function() {
+    var form = document.getElementById('myForm');
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var action = form.getAttribute('action');
+        window.location.href = action;
     });
 });
+
     
